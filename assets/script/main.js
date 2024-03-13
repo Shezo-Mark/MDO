c545599e6537657ce1061e7289ba1da6b5e5e266
Hamza-A-Rasheed <146958703+Hamza-Rasheed-25@users.noreply.github.com>
2024-03-14 02:35:00 +0500

    homepage nearly completed and slider updated

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,61 +1,55 @@
 var $owl = $('.comprehensive_coverage_slider');
 
-$owl.children().each( function( index ) {
-  $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+$owl.children().each(function (index) {
+    $(this).attr('data-position', index); // NB: .attr() instead of .data()
 });
 
 $owl.owlCarousel({
-  center: true,
-  loop: true,
-  responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+    center: true,
+    loop: true,
+    nav:false,
+    dots:true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        480:{
-            items:1.5,
-            nav:true
+        480: {
+            items: 1.5
         },
-        768:{
-            items:2.5,
-            nav:true,
-            loop:true
+        768: {
+            items: 2.5
         },
-        1024:{
-            items:3.5,
-            nav:true,
-            loop:true
+        1024: {
+            items: 3.5
         },
-        1440:{
-            items:4.5,
-            nav:true,
+        1440: {
+            items: 5.5
         },
-        1990:{
-            items:5.5,
-            nav:true,
-                    }
+        1990: {
+            items: 7.5
+        }
     }
 });
 
-$(document).on('click', '.owl-item>div', function() {
-  // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
-  var $speed = 300;  // in ms
-  $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
+$(document).on('click', '.owl-item>div', function () {
+    // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
+    var $speed = 300;  // in ms
+    $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]);
 });
 $('.stat-number').each(function () {
     var size = $(this).text().split(".")[1] ? $(this).text().split(".")[1].length : 0;
     $(this).prop('Counter', 0).animate({
-       Counter: $(this).text()
+        Counter: $(this).text()
     }, {
-       duration: 5000,
-       step: function (func) {
-          $(this).text(parseFloat(func).toFixed(size));
-       }
+        duration: 3000,
+        step: function (func) {
+            $(this).text(parseFloat(func).toFixed(size));
+        }
     });
- });
+});
 
- 
+
 $('.test_slider').owlCarousel({
     loop: true,
     margin: 10,
