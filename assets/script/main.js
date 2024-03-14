c6efbd8cbeaad211990b8301dac057afc847dca2
shehzad <shehzadamin697@gmail.com>
2024-03-14 12:47:32 +0500

    small changes

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,71 +1,120 @@
-var $owl = $('.comprehensive_coverage_slider');
-
-$owl.children().each(function (index) {
-    $(this).attr('data-position', index); // NB: .attr() instead of .data()
-});
-
-$owl.owlCarousel({
-    center: true,
-    loop: true,
-    nav:false,
-    dots:true,
-    responsiveClass: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        480: {
-            items: 1.5
-        },
-        768: {
-            items: 2.5
-        },
-        1024: {
-            items: 3.5
-        },
-        1440: {
-            items: 5.5
-        },
-        1990: {
-            items: 7.5
-        }
-    }
-});
-
-$(document).on('click', '.owl-item>div', function () {
-    // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
-    var $speed = 300;  // in ms
-    $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]);
-});
 $('.stat-number').each(function () {
     var size = $(this).text().split(".")[1] ? $(this).text().split(".")[1].length : 0;
     $(this).prop('Counter', 0).animate({
         Counter: $(this).text()
     }, {
-        duration: 3000,
+        duration: 10000,
         step: function (func) {
             $(this).text(parseFloat(func).toFixed(size));
         }
     });
 });
 
+$( ".custom_package" ).on( "click", function( event ) {
+    event.preventDefault();
+    $('.filter_data').removeClass('is_checked');
+    $(".custom_package").addClass('is_checked');  
+  });
+  $( ".research_package" ).on( "click", function( event ) {
+    event.preventDefault();
+    $('.filter_data').removeClass('is_checked');
+    $(".research_package").addClass('is_checked');  
+  });
+  $( ".ultimate_package" ).on( "click", function( event ) {
+    event.preventDefault();
+    $('.filter_data').removeClass('is_checked');
+    $(".ultimate_package").addClass('is_checked');  
+  });
 
-$('.test_slider').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    responsive: {
-        0: {
-            nav: true,
-            items: 1
-        },
-        1000: {
-            nav: true,
-            items: 2
-        },
-        1400: {
-            nav: true,
-            items: 3.5
+
+
+
+// var $owl = $('.comprehensive_coverage_slider');
+
+// $owl.children().each(function (index) {
+//     $(this).attr('data-position', index); // NB: .attr() instead of .data()
+// });
+
+// $owl.owlCarousel({
+//     center: true,
+//     loop: true,
+//     nav:false,
+//     dots:true,
+//     responsiveClass: true,
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         480: {
+//             items: 1.5
+//         },
+//         768: {
+//             items: 2.5
+//         },
+//         1024: {
+//             items: 3.5
+//         },
+//         1440: {
+//             items: 5.5
+//         },
+//         1990: {
+//             items: 7.5
+//         }
+//     }
+// });
+
+// $(document).on('click', '.owl-item>div', function () {
+//     // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
+//     var $speed = 300;  // in ms
+//     $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]);
+// });
+
+
+
+// $('.test_slider').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     nav: true,
+//     responsive: {
+//         0: {
+//             nav: true,
+//             items: 1
+//         },
+//         1000: {
+//             nav: true,
+//             items: 2
+//         },
+//         1400: {
+//             nav: true,
+//             items: 3.5
+//         }
+//     }
+// })
+
+
+
+  $('.comprehensive_coverage_slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 5,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
         }
-    }
-})
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
