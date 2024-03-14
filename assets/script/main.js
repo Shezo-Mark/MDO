1c68c7c56c46e0861deb57c05aa5c960e31a92e7
shehzad <shehzadamin697@gmail.com>
2024-03-14 20:07:38 +0500

    content update

--- assets/script/main.js
+++ assets/script/main.js
@@ -29,92 +29,95 @@ $( ".custom_package" ).on( "click", function( event ) {
 
 
 
-// var $owl = $('.comprehensive_coverage_slider');
+var $owl = $('.comprehensive_coverage_slider');
 
-// $owl.children().each(function (index) {
-//     $(this).attr('data-position', index); // NB: .attr() instead of .data()
-// });
+$owl.children().each(function (index) {
+    $(this).attr('data-position', index); // NB: .attr() instead of .data()
+});
 
-// $owl.owlCarousel({
-//     center: true,
-//     loop: true,
-//     nav:false,
-//     dots:true,
-//     responsiveClass: true,
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-//         480: {
-//             items: 1.5
-//         },
-//         768: {
-//             items: 2.5
-//         },
-//         1024: {
-//             items: 3.5
-//         },
-//         1440: {
-//             items: 5.5
-//         },
-//         1990: {
-//             items: 7.5
-//         }
-//     }
-// });
-
-// $(document).on('click', '.owl-item>div', function () {
-//     // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
-//     var $speed = 300;  // in ms
-//     $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]);
-// });
-
-
-
-// $('.test_slider').owlCarousel({
-//     loop: true,
-//     margin: 10,
-//     nav: true,
-//     responsive: {
-//         0: {
-//             nav: true,
-//             items: 1
-//         },
-//         1000: {
-//             nav: true,
-//             items: 2
-//         },
-//         1400: {
-//             nav: true,
-//             items: 3.5
-//         }
-//     }
-// })
-
-
-
-  $('.comprehensive_coverage_slider').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 5,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
+$owl.owlCarousel({
+    center: true,
+    loop: true,
+    nav:false,
+    dots:true,
+    responsiveClass: true,
+    autoplay:true,
+autoplayTimeout:3000,
+autoplayHoverPause:true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        480: {
+            items: 1.5
+        },
+        768: {
+            items: 2.5
+        },
+        1024: {
+            items: 3.5
+        },
+        1440: {
+            items: 5.5
+        },
+        1990: {
+            items: 7.5
         }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
+    }
+});
+
+$(document).on('click', '.owl-item>div', function () {
+    // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
+    var $speed = 300;  // in ms
+    $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]);
+});
+
+
+
+$('.test_slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            nav: true,
+            items: 1
+        },
+        1000: {
+            nav: true,
+            items: 2
+        },
+        1400: {
+            nav: true,
+            items:4,
         }
-      }
-    ]
-  });
+    }
+})
+
+
+
+//   $('.comprehensive_coverage_slider').slick({
+//     centerMode: true,
+//     centerPadding: '60px',
+//     slidesToShow: 5,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
